3ed49ce2e1bfec052fc19a2327c81df3e3d82bfd
Edwin-Antonio <edanarellanoma@ittepic.edu.mx>
2021-03-16 09:14:23 -0700

    Initial commit

---
+++ app/src/main/java/mx/tecnm/tepic/ladm_u1_practica1_layouts/MainActivity.kt
@@ -0,0 +1,37 @@
+package mx.tecnm.tepic.ladm_u1_practica1_layouts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        menuprincipal.setOnItemClickListener { parent, view, i, id ->
+            when(i){
+                0->{
+                    var activity2 = Intent(this, MainActivity2::class.java)
+                    startActivity(activity2)
+                }
+                1->{
+                    var activity3 = Intent(this, MainActivity3::class.java)
+                    startActivity(activity3)
+                }
+                2->{
+                    var activity4 = Intent(this, MainActivity4::class.java)
+                    startActivity(activity4)
+                }
+                3->{
+                    var activity5 = Intent(this, MainActivity5::class.java)
+                    startActivity(activity5)
+                }
+                4->{
+                    finish()
+                }
+            }
+        }
+    }
+}
